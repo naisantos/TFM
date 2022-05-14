0341c8c36001995aa6690acc94070a751b27ff81
naisantos <75383897+naisantos@users.noreply.github.com>
2022-05-14 13:32:46 +0200

    Add files via upload

---
+++ H3K4me3_nRC_download.sh
@@ -0,0 +1,22 @@
+wget -b -c --user=ClaudiaBigas_AZNARSAL --password=drc-HAyn http://seq.crg.es/download/external/Miro/Salvador_Aznar-Benitah/Claudia_Bigas/2022-03-01_BCDB2VANXX/AZNARSAL_49/CF3_1PN_me3_01350AAD_ACTTGA_R1_001.fastq.gz &
+wget -b -c --user=ClaudiaBigas_AZNARSAL --password=drc-HAyn http://seq.crg.es/download/external/Miro/Salvador_Aznar-Benitah/Claudia_Bigas/2022-03-01_BCDB2VANXX/AZNARSAL_49/CF3_2PN_me3_01351AAD_ATGTCA_R1_001.fastq.gz &
+wget -b -c --user=ClaudiaBigas_AZNARSAL --password=drc-HAyn http://seq.crg.es/download/external/Miro/Salvador_Aznar-Benitah/Claudia_Bigas/2022-03-01_BCDB2VANXX/AZNARSAL_49/CF3_3PN_me3_01352AAD_GAGTGG_R1_001.fastq.gz &
+wget -b -c --user=ClaudiaBigas_AZNARSAL --password=drc-HAyn http://seq.crg.es/download/external/Miro/Salvador_Aznar-Benitah/Claudia_Bigas/2022-03-01_BCDB2VANXX/AZNARSAL_49/CM3_1PN_me3_01347AAD_TTAGGC_R1_001.fastq.gz &
+wget -b -c --user=ClaudiaBigas_AZNARSAL --password=drc-HAyn http://seq.crg.es/download/external/Miro/Salvador_Aznar-Benitah/Claudia_Bigas/2022-03-01_BCDB2VANXX/AZNARSAL_49/CM3_2PN_me3_01348AAD_TGACCA_R1_001.fastq.gz &
+wget -b -c --user=ClaudiaBigas_AZNARSAL --password=drc-HAyn http://seq.crg.es/download/external/Miro/Salvador_Aznar-Benitah/Claudia_Bigas/2022-03-01_BCDB2VANXX/AZNARSAL_49/CM3_3PN_me3_01349AAD_GCCAAT_R1_001.fastq.gz &
+wget -b -c --user=ClaudiaBigas_AZNARSAL --password=drc-HAyn http://seq.crg.es/download/external/Miro/Salvador_Aznar-Benitah/Claudia_Bigas/2022-03-01_BCDB2VANXX/AZNARSAL_49/FastQC.tar.gz &
+wget -b -c --user=ClaudiaBigas_AZNARSAL --password=drc-HAyn http://seq.crg.es/download/external/Miro/Salvador_Aznar-Benitah/Claudia_Bigas/2022-03-01_BCDB2VANXX/AZNARSAL_49/PF3_1PN_me3_01356AAD_CACGAT_R1_001.fastq.gz &
+wget -b -c --user=ClaudiaBigas_AZNARSAL --password=drc-HAyn http://seq.crg.es/download/external/Miro/Salvador_Aznar-Benitah/Claudia_Bigas/2022-03-01_BCDB2VANXX/AZNARSAL_49/PF3_2PN_me3_01357AAD_CGGAAT_R1_001.fastq.gz &
+wget -b -c --user=ClaudiaBigas_AZNARSAL --password=drc-HAyn http://seq.crg.es/download/external/Miro/Salvador_Aznar-Benitah/Claudia_Bigas/2022-03-01_BCDB2VANXX/AZNARSAL_49/PF3_3PN_me3_01358AAD_TCATTC_R1_001.fastq.gz &
+wget -b -c --user=ClaudiaBigas_AZNARSAL --password=drc-HAyn http://seq.crg.es/download/external/Miro/Salvador_Aznar-Benitah/Claudia_Bigas/2022-03-01_BCDB2VANXX/AZNARSAL_49/PM3_1PN_me3_01353AAD_GGTAGC_R1_001.fastq.gz &
+wget -b -c --user=ClaudiaBigas_AZNARSAL --password=drc-HAyn http://seq.crg.es/download/external/Miro/Salvador_Aznar-Benitah/Claudia_Bigas/2022-03-01_BCDB2VANXX/AZNARSAL_49/PM3_2PN_me3_01354AAD_ATTCCT_R1_001.fastq.gz &
+wget -b -c --user=ClaudiaBigas_AZNARSAL --password=drc-HAyn http://seq.crg.es/download/external/Miro/Salvador_Aznar-Benitah/Claudia_Bigas/2022-03-01_BCDB2VANXX/AZNARSAL_49/PM3_3PN_me3_01355AAD_CACCGG_R1_001.fastq.gz &
+
+
+
+
+
+
+
+
+
