cc8b73b704e10d7bd95a95e80c89bf233024dd16
naisantos <75383897+naisantos@users.noreply.github.com>
2022-05-14 13:41:20 +0200

    Add files via upload

---
+++ fastqc.sh
@@ -0,0 +1,15 @@
+##Ver la calidad de las muestras de trimming
+
+##H3K4me3
+#El siguiente comando es lo que se debería escribir en la terminal
+#FASTQC - A high throughput sequence QC analysis tool
+# DESCRIPTION: FastQC reads a set of sequence files and produces from each one a quality control report consisting of a number of different modules, each one of which will help to identify a different potential type of problem in your data.
+
+visitor6@sequentia:/media/sequentia/synology_office/Naiara/H3K4me3/Trimmed_output/Trimmed_files$ fastqc  -o ./Trimmed_QC CF3_1PN_me3_01350AAD_ACTTGA_R1_001.tr.fq.gz  CF3_2PN_me3_01351AAD_ATGTCA_R1_001.tr.fq.gz CF3_3PN_me3_01352AAD_GAGTGG_R1_001.tr.fq.gz  CM3_1PN_me3_01347AAD_TTAGGC_R1_001.tr.fq.gz CM3_2PN_me3_01348AAD_TGACCA_R1_001.tr.fq.gz  CM3_3PN_me3_01349AAD_GCCAAT_R1_001.tr.fq.gz  PF3_1PN_me3_01356AAD_CACGAT_R1_001.tr.fq.gz  PF3_2PN_me3_01357AAD_CGGAAT_R1_001.tr.fq.gz  PF3_3PN_me3_01358AAD_TCATTC_R1_001.tr.fq.gz  PM3_1PN_me3_01353AAD_GGTAGC_R1_001.tr.fq.gz  PM3_2PN_me3_01354AAD_ATTCCT_R1_001.tr.fq.gz  PM3_3PN_me3_01355AAD_CACCGG_R1_001.tr.fq.gz  &
+
+
+
+
+
+
+
